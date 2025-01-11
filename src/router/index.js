4337ac8d5ed88eb32d9ed185719acd1410426c69
87Meng <tsaimeng1225@gmail.com>
2025-01-11 17:45:32 +0800

    test..

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import("../views/AboutView.vue"),
+    // },
 
-export default router
+    // 景點介紹
+    { path: "/", name: "景點介紹" },
+
+    // 積分任務
+    { path: "/", name: "一般任務" },
+    { path: "/", name: "特殊任務" },
+
+    // 會員中心
+    { path: "/user-profile", name: "UserProfilePage" },
+    { path: "/password", name: "PasswordPage" },
+    { path: "/favorites", name: "FavoritesPage" },
+    { path: "/cart", name: "CartPage" },
+
+    // 商城
+    { path: "/", name: "商城" },
+
+    // 最新消息
+    { path: "/news", name: "News" },
+  ],
+});
+
+export default router;
