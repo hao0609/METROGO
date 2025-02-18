5fca9df7fdc670a33144f22fde5e762594e519ff
ytlee74 <ellen70733@gmail.com>
2025-02-18 21:19:19 +0800

    更新 entrance 畫面

--- src/views/EntranceView.vue
+++ src/views/EntranceView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="entrance-container">
+  <!-- <div class="container">
     <div class="back">
       <img class="vector-left" src="../assets/images/Entrance_Vector_left.svg" alt="" />
       <img class="vector-right" src="../assets/images/Entrance_Vector_right.svg" alt="" />
@@ -25,6 +25,33 @@
           <RouterLink to="/backend-login">
             <img src="../assets/images/Entrance_BackEnd.svg" alt="前往後台" />
           </RouterLink>
+        </div>
+      </div>
+    </div>
+  </div> -->
+
+  <div class="entrance-container">
+    <div class="bg-container"></div>
+    <div class="content">
+      <div class="front_logo">
+        <img src="../assets/images/Entrance_Logo.svg" alt="" />
+      </div>
+      <div class="entrance-info">
+        <div class="entrance-arrow">
+          <div class="left">
+            <RouterLink to="/sights">
+              <img src="../assets/images/Entrance_FrontEnd.svg" alt="前往前台" />
+            </RouterLink>
+          </div>
+          <div class="metro">
+            <img class="small" src="../assets/images/Entrance_metro_small.svg" />
+            <img class="big" src="../assets/images/Entrance_metro_big.svg" alt="" />
+          </div>
+          <div class="left">
+            <RouterLink to="/backend-login">
+              <img src="../assets/images/Entrance_BackEnd.svg" alt="前往後台" />
+            </RouterLink>
+          </div>
         </div>
       </div>
     </div>
