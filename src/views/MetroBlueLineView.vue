a3b94e4d6229164543d6f0841a455a24634cb331
87Meng <tsaimeng1225@gmail.com>
2025-02-20 16:38:35 +0800

    板南線

--- src/views/MetroBlueLineView.vue
+++ src/views/MetroBlueLineView.vue
@@ -1,20 +1,163 @@
 <template>
-    
-    <div class="blueline_introduce">
-
-        <div class="bl_intr_banner_01">
-          <div class="bl_inter_banner_01a">
-            <img class="bl_10_01" src="../assets/images/sample.jpg"
-            width="300px" hight="417px">
+  <div class="metroline-detail blue">
+    <div class="introduce">
+      <div class="banner-group group-1">
+        <div class="banner">
+          <div class="banner-img">
+            <img src="https://picsum.photos/300/417" />
           </div>
-          <div class="b1_inter_banner_01_1">
-            <p class="title1 Bold">有拜有保庇，你想要..?</p>
-            <p class="title2 Bold">懷著滿滿的願望，誠懇地與神交流吧！充滿歷史意義與神秘的氛圍，不管你想得到
-            甚麼或想知道甚麼，甚至煩惱糾結的選擇，這裡都可以給你參考的答案！</p>
+          <div class="banner-txt-wrapper">
+            <p class="title1 bold banner-txt-title">有拜有保庇，你想要..?</p>
+            <p class="title2 bold banner-txt-content">
+              懷著滿滿的願望，誠懇地與神交流吧！充滿歷史意義與神秘的氛圍，不管你想得到
+              甚麼或想知道甚麼，甚至煩惱糾結的選擇，這裡都可以給你參考的答案！
+            </p>
           </div>
         </div>
+        <div class="banner">
+          <div class="banner-img">
+            <img src="https://picsum.photos/300/417" />
+          </div>
+          <div class="banner-txt-wrapper">
+            <p class="title1 bold banner-txt-title">有拜有保庇，你想要..?</p>
+            <p class="title2 bold banner-txt-content">
+              懷著滿滿的願望，誠懇地與神交流吧！充滿歷史意義與神秘的氛圍，不管你想得到
+              甚麼或想知道甚麼，甚至煩惱糾結的選擇，這裡都可以給你參考的答案！
+            </p>
+          </div>
+        </div>
+      </div>
 
-        <div class="bl_inter_banner_02">
+      <div class="banner-group group-2">
+        <div class="banner">
+          <div class="banner-img">
+            <img src="https://picsum.photos/300/417" />
+          </div>
+          <div class="banner-txt-wrapper">
+            <p class="title1 bold banner-txt-title">有拜有保庇，你想要..?</p>
+            <p class="title2 bold banner-txt-content">
+              懷著滿滿的願望，誠懇地與神交流吧！充滿歷史意義與神秘的氛圍，不管你想得到
+              甚麼或想知道甚麼，甚至煩惱糾結的選擇，這裡都可以給你參考的答案！
+            </p>
+          </div>
+        </div>
+        <h1 class="banner-group-title bl">板南線</h1>
+        <div class="banner">
+          <div class="banner-img">
+            <img src="https://picsum.photos/300/417" />
+          </div>
+          <div class="banner-txt-wrapper">
+            <p class="title1 bold banner-txt-title">有拜有保庇，你想要..?</p>
+            <p class="title2 bold banner-txt-content">
+              懷著滿滿的願望，誠懇地與神交流吧！充滿歷史意義與神秘的氛圍，不管你想得到
+              甚麼或想知道甚麼，甚至煩惱糾結的選擇，這裡都可以給你參考的答案！
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="banner-group group-3">
+        <div class="banner">
+          <div class="banner-img">
+            <img src="https://picsum.photos/300/417" />
+          </div>
+          <div class="banner-txt-wrapper">
+            <p class="title1 bold banner-txt-title">有拜有保庇，你想要..?</p>
+            <p class="title2 bold banner-txt-content">
+              懷著滿滿的願望，誠懇地與神交流吧！充滿歷史意義與神秘的氛圍，不管你想得到
+              甚麼或想知道甚麼，甚至煩惱糾結的選擇，這裡都可以給你參考的答案！
+            </p>
+          </div>
+        </div>
+        <div class="banner">
+          <div class="banner-img">
+            <img src="https://picsum.photos/300/417" />
+          </div>
+          <div class="banner-txt-wrapper">
+            <p class="title1 bold banner-txt-title">有拜有保庇，你想要..?</p>
+            <p class="title2 bold banner-txt-content">
+              懷著滿滿的願望，誠懇地與神交流吧！充滿歷史意義與神秘的氛圍，不管你想得到
+              甚麼或想知道甚麼，甚至煩惱糾結的選擇，這裡都可以給你參考的答案！
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- GO精選 -->
+    <div class="metro-line-title">
+      <h1 class="">GO精選</h1>
+    </div>
+    <div class="section-group">
+      <div class="go-choise-img w-half">
+        <h2 class="group-title">火鍋季開跑</h2>
+      </div>
+      <div class="go-choise-img w-half">
+        <h2 class="group-title">耶誕城派對</h2>
+      </div>
+    </div>
+
+    <!-- 半日遊行程推薦 -->
+    <div class="metro-line-title">
+      <h1>板南線半日遊行程推薦</h1>
+    </div>
+    <div class="w-full-img">
+      <h1 class="group-title white">西門町</h1>
+    </div>
+
+    <!-- 一日遊行程推薦 -->
+    <div class="metro-line-title">
+      <h1>板南線半日遊行程推薦</h1>
+    </div>
+    <div class="w-full-img">
+      <h1 class="group-title white">龍山寺</h1>
+    </div>
+
+    <!-- 留言板 -->
+    <div class="metro-line-title">
+      <h1>留言板</h1>
+    </div>
+    <div class="section-group">
+      <div class="w-half">
+        <div class="review-card">
+          <div class="review-header">
+            <img
+              src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Ccircle cx='12' cy='12' r='12' fill='%23E0E0E0'/%3E%3Cpath d='M12 14c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z' fill='%23999'/%3E%3C/svg%3E"
+              alt="用戶頭像"
+              class="user-avatar"
+            />
+            <div class="user-info">
+              <span class="user-name">吳楚仁</span>
+              <span class="rating">★★★</span>
+            </div>
+          </div>
+          <div class="review-content">
+            下午接近黃昏的海山站，別有一番特色，不管來散步或者要去新北高工去運動，都是不錯的選擇，捷運站附近也有不錯好吃的點心店，綠化也相當不錯，也有行人徒步區，輕鬆好逛不費力！
+          </div>
+          <div class="review-images">
+            <img
+              src="https://picsum.photos/200/400"
+              alt="海山站景色 1"
+              class="review-image"
+            />
+            <img
+              src="https://picsum.photos/200/400"
+              alt="海山站景色 2"
+              class="review-image"
+            />
+            <img
+              src="https://picsum.photos/200/400"
+              alt="海山站景色 3"
+              class="review-image"
+            />
+          </div>
+          <div class="timestamp">1小時前</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="bl_inter_banner_02">
           <div class="bl_inter_banner_02a">
             <img class="bl_14_01" src="../assets/images/sample.jpg"
             width="300px" hight="417px">
@@ -103,15 +246,9 @@
 
         <div class="bl_halfday_travel_01">
             <img class="bl_10_02" src="../assets/images/sample.jpg">
-        </div>
-
-    </div>
-
-
-
-
+        </div> -->
 </template>
 
 <style lang="scss" scoped>
-    @use "../assets/sass/page/_metro-line.scss"
+@use "../assets/sass/page/_metro-line.scss";
 </style>
