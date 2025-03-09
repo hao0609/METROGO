013c82ecd11b5fa2e424b5c748b988f60f59ba87
amber_07 <amberrr001@gmail.com>
2025-03-09 16:37:01 +0800

    1. 試用 alertM 在會員中心 2. 把 lighten, darken 樣式先寫死 hex碼 (不然一直跳警告QQ)

--- src/views/UserProfileView.vue
+++ src/views/UserProfileView.vue
@@ -59,7 +59,7 @@
                         <input type="text" class="input-field" v-model="userEmail" disabled>
                     </div>
                 </div>
-                <button class="btn_filled">更新資料</button>
+                <button class="btn_filled" @click="showUpdateAlert">更新資料</button>
             </div>
         </div>
         
@@ -202,7 +202,8 @@
                 </div>
             </div>
         </div>
-    </div>    
+    </div>   
+    <Alert_web_M ref="alertM" :alertInfo="alertInfo" />
 </template>
 
 <style lang="scss" scoped>
@@ -217,7 +218,7 @@ import EyeIcon from '@/components/icons/IconEye.vue';
 import HearFillIcon from '@/components/icons/IconHeartfill.vue';
 import CartIcon from '@/components/icons/IconCart.vue';
 // 彈窗
-// import Alert_web_M from '@/components/Alert_web_M.vue';
+import Alert_web_M from '@/components/Alert_web_M.vue';
 import Swal from 'sweetalert2';
 
 
@@ -229,7 +230,7 @@ export default {
         EyeIcon,
         HearFillIcon,
         CartIcon,
-        //Alert_web_M,
+        Alert_web_M,
     },
     data() {
         return {
@@ -263,19 +264,25 @@ export default {
                         }
                     },
                     ],
-            //// Alert_M
-            // alertInfo: {
-            // fristTitle: '訂單詳情',
-            // svg_icon: `...`,
-            // SecondTittle: '',
-            // ThirdTittle: '',
-            // ButtonText: '確認',
-            // allowOutsideClick: true,
-            // function: () => {
-            //     console.log('Alert 確認按鈕被點擊');
-            // }
-            
-        }
+            // Alert_M
+            alertInfo: {
+            fristTitle: '資料更新',
+            svg_icon: `<svg width="160" height="160" viewBox="0 0 160 160" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="80" cy="80" r="75" stroke="#7D8A93" stroke-width="10"/>
+            <path d="M63.2393 73.1818C63.2393 76.88 64.7084 80.4268 67.3234 83.0418C69.9385 85.6569 73.4852 87.126 77.1835 87.126C80.8817 87.126 84.4285 85.6569 87.0435 83.0418C89.6586 80.4268 91.1277 76.88 91.1277 73.1818C91.1277 69.4835 89.6586 65.9368 87.0435 63.3217C84.4285 60.7067 80.8817 59.2375 77.1835 59.2375C73.4852 59.2375 69.9385 60.7067 67.3234 63.3217C64.7084 65.9368 63.2393 69.4835 63.2393 73.1818Z" stroke="#7D8A93" stroke-width="10" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M91.1134 111.84L83.7555 119.198C82.0124 120.939 79.6493 121.917 77.1854 121.917C74.7215 121.917 72.3585 120.939 70.6154 119.198L50.8889 99.4759C46.5553 95.1416 43.3603 89.8033 41.5882 83.9359C39.8161 78.0685 39.5217 71.8541 40.7315 65.8455C41.9412 59.8369 44.6174 54.2205 48.522 49.4961C52.4266 44.7716 57.4384 41.0856 63.1117 38.7659C68.7849 36.4463 74.9436 35.565 81.0397 36.2004C87.1358 36.8358 92.9803 38.9683 98.0533 42.4081C103.126 45.8479 107.27 50.4883 110.116 55.9164C112.963 61.3446 114.423 67.3922 114.368 73.5211M109.72 124.311V124.357M109.72 110.366C111.803 110.36 113.825 109.654 115.46 108.363C117.095 107.071 118.25 105.269 118.739 103.243C119.229 101.218 119.025 99.0868 118.16 97.1912C117.295 95.2955 115.819 93.7448 113.968 92.7873C112.119 91.8401 110.004 91.5464 107.966 91.954C105.929 92.3616 104.09 93.4466 102.748 95.0324" stroke="#7D8A93" stroke-width="10" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            `, //無法確認 ICON 是否可以用外部引入.vue檔方式，目前先使用字串方式`,
+            SecondTittle: '副標文字',
+            ThirdTittle: '副副標文字',
+            ButtonText: '確認',
+            allowOutsideClick: true,
+            function: () => {
+                console.log('Alert 確認按鈕被點擊');
+            }
+        }  
+    };  
+        
     },
     methods: {
         handleMenuClick(menuId) {
@@ -283,6 +290,10 @@ export default {
         },
         handleMobileMenuChange(event) {
             this.activeMenuItem = event.target.value;
+        },
+        // 新增顯示資料更新提示的方法
+        showUpdateAlert() {
+            this.$refs.alertM.showAlert();
         },
         // 訂單詳情談窗
         handleOrderDetails(orderId) {
