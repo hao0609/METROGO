752cf34a801e592d097df6bd12f7cab0987dc2fc
ytlee74 <ellen70733@gmail.com>
2025-03-31 21:33:24 +0800

    更新 圖片

--- src/components/FeaturedMain.vue
+++ src/components/FeaturedMain.vue
@@ -56,13 +56,65 @@
 </template>
 
 <script setup>
-// 接收來自父組件的 props
 defineProps({
-  FeaturedMain: {
-    type: Object,
-    required: true, // 確保這個 prop 是必要的
-  },
+  FeaturedMain: Object, // 接收來自父元件的標題圖片
 });
+
+// import { ref } from "vue";
+
+// const props = defineProps({
+//   FeaturedMain: Object,
+// });
+
+// // **主要圖片**
+// const mainPhoto = ref({
+//   src: props.FeaturedMain.featured_main_photo,
+//   alt: "主圖片",
+//   type: "main",
+//   width: "auto", // 默認寬度
+//   height: "auto", // 默認高度
+// });
+
+// // **側邊圖片 (前兩張)**
+// const sidePhotos = ref(
+//   props.FeaturedMain.featured_photos.slice(0, 2).map((src, index) => ({
+//     src,
+//     alt: `側邊圖片${index + 1}`,
+//     type: "side",
+//   }))
+// );
+
+// // **底部圖片 (剩下的)**
+// const bottomPhotos = ref(
+//   props.FeaturedMain.featured_photos.slice(2).map((src, index) => ({
+//     src,
+//     alt: `底部圖片${index + 3}`,
+//     type: "bottom",
+//   }))
+// );
+
+// // **圖片交換函式**
+// const swapImage = (clickedPhoto, fromType, index) => {
+//   let temp = { ...mainPhoto.value }; // 暫存主圖片
+
+//   // 根據點擊的圖片來更新主圖片的大小
+//   const clickedImage = new Image();
+//   clickedImage.src = clickedPhoto.src;
+//   clickedImage.onload = () => {
+//     mainPhoto.value.width = `${clickedImage.width}px`;
+//     mainPhoto.value.height = `${clickedImage.height}px`;
+//   };
+
+//   // 交換點擊的圖片和主圖片
+//   if (fromType === "side") {
+//     sidePhotos.value[index] = temp;
+//   } else if (fromType === "bottom") {
+//     bottomPhotos.value[index] = temp;
+//   }
+
+//   // 更新主圖片
+//   mainPhoto.value = clickedPhoto;
+// };
 </script>
 
 <style lang="scss" scoped>
